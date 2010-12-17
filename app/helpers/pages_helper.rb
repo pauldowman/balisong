ce366f35d5062e96e682c40c509516df0ecbd325
Gianni Chiappetta <gianni@runlevel6.org>
2010-12-16 21:01:04 -0500

    removed: SyntaxHighlighter.

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -33,7 +33,7 @@ module PagesHelper
       out = "<pre>#{html_escape(raw_data)}</pre>"
     when "code"
       lang = formatter_arg || Page.type(part_name)
-      out = "<pre class='brush: #{lang}'>#{html_escape(raw_data)}</pre>"
+      out = "<pre><code class='#{lang}'>#{html_escape(raw_data)}<code></pre>"
     when "image"
       # TODO use url_for here
       out = "<img src='/#{page.id}/#{part_name}'>"
