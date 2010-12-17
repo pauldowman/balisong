8b4a65887047b7bae6b9f4b6c4439e8bc83930bd
Gianni Chiappetta <gianni@runlevel6.org>
2010-12-17 15:54:54 -0500

    fixed: Render sub-parts first.

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -1,5 +1,7 @@
 module PagesHelper
 
+  @@skip = [ :image, :download ]
+
   # Render a part as HTML. Name is the filename of the part, and formatter is
   # the name of the formatter that will be used to render the part as HTML,
   # something like "code" or "html" or "code(ruby)".
@@ -16,10 +18,17 @@ module PagesHelper
       formatter_arg = nil
     end
 
-    Rails.logger.info "Rendering part: #{page}/#{part_name} with formatter #{formatter} (with arg: #{formatter_arg})"
-
     raw_data = page.blobs[part_name]
     raise "Can't find part '#{part_name}'" unless raw_data
+
+    # Render sub-parts recursively
+    unless @@skip.include? formatter.to_sym
+      # match {{ file.md }} or {{file.rb | code(ruby)}}, etc.
+      regex = /\{\{\s*([\w\.-]+)\s*\|?\s*([\w\.-]+\(?[\w\.-]*\)?)?\s*\}\}/
+      raw_data = raw_data.gsub(regex) {|match| render_part(page, $1, $2) }
+    end
+
+    Rails.logger.info "Rendering part: #{page}/#{part_name} with formatter #{formatter} (with arg: #{formatter_arg})"
 
     # these should be extracted somewhere to be extensible
     case formatter
@@ -33,24 +42,15 @@ module PagesHelper
       out = "<pre>#{html_escape(raw_data)}</pre>"
     when "code"
       lang = formatter_arg || Page.type(part_name)
-      out = "<pre><code class='#{lang}'>#{html_escape(raw_data)}<code></pre>"
+      out = "<pre><code class='#{lang}'>#{html_escape(raw_data)}</code></pre>"
     when "image"
       # TODO use url_for here
       out = "<img src='/#{page.id}/#{part_name}'>"
-      skip_sub_render = true
     when "download"
       # TODO use url_for here
       out = link_to(part_name, "/#{page.id}/#{part_name}?download=true")
-      skip_sub_render = true
     else
       raise "unknown formatter: #{formatter}"
-    end
-
-    # Render sub-parts recursively
-    unless skip_sub_render
-      # match {{ file.md }} or {{file.rb | code(ruby)}}, etc.
-      regex = /\{\{\s*([\w\.-]+)\s*\|?\s*([\w\.-]+\(?[\w\.-]*\)?)?\s*\}\}/
-      out = out.gsub(regex) {|match| render_part(page, $1, $2) }
     end
 
     return out
