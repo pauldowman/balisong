edbb90691f18459774e06999e1ffa94d819046b3
Paul Dowman <paul@pauldowman.com>
2011-07-07 15:07:24 -0400

    Add 404 page, and GitModel::RecordNotFound renders 404

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,18 @@ class ApplicationController < ActionController::Base
 
   before_filter :populate_site_menu
 
+  rescue_from GitModel::RecordNotFound, :with => :render_404
+ 
+  private
+
+  def render_404
+    # Log a warning if there was a 404 from a link on our own site
+    if request.referer =~ /#{request.env["SERVER_NAME"]}/
+      Rails.logger.warn "Page not found: #{request.path}, referer was #{request.referer}"
+    end
+    render :template => "errors/404", :status => 404
+  end
+
   def populate_site_menu
     @site_menu = Page.find_all(:site_menu_position => lambda{|x| !x.nil?}, :order_by => :site_menu_position, :order => :asc)
   end
